2b08fef3ca727f6d0801de6ad5d71e3f17967b92
huyvo <huyvanvo94@gmail.com>
2017-11-08 17:02:05 -0800

    finish lab

--- public_html/contacts.php
+++ public_html/contacts.php
@@ -52,7 +52,20 @@
 
 
 <body>
- 
+
+<div id="nav">
+    <div id="nav-wrapper">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="news.html">News</a></li>
+            <li><a href="contacts.php">Contacts</a></li>
+            <li><a href="service.html">Services</a></li>
+            <li><a href="login-admin.html">Secure</a> </li>
+            <li><a href="user-choose.html">User</a> </li>
+        </ul>
+    </div>
+</div>
 
 <div id="contact-container">
     <h3>Contact Information</h3>
